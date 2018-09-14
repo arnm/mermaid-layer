716aa59f5ca4d7f2f62e6d7bc1aff610d18f90ff
Alexei Nunez <alexei.nunez@bazaarvoice.com>
2018-09-14 14:29:30 -0500

    init

---
+++ packages.el
@@ -0,0 +1,23 @@
+;;; packages.el --- mermaid layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2018 Alexei Nunez
+;;
+;; Author: Alexei Nunez <alexeirnunez@gmail.com>
+;;
+;;; Commentary:
+
+;; Org-Babel support for evaluating mermaid diagrams in Spacemacs.
+;;
+;;; Requirements:
+
+;; ob-mermaid | https://github.com/arnm/ob-mermaid
+;; mermaid.cli | https://github.com/mermaidjs/mermaid.cli
+
+;;; Code:
+(defconst mermaid-packages
+  '(org
+    ob-mermaid))
+
+(defun mermaid/post-init-org ()
+  (spacemacs|use-package-add-hook org
+    :post-config (add-to-list 'org-babel-load-languages '(mermaid . t))))
